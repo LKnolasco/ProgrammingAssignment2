e51ca31dcee78c38d069a352d529b534019eb039
LKnolasco <82467178+LKnolasco@users.noreply.github.com>
2021-04-19 14:27:55 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,16 @@
-## the task is to cache the inverse of a matrix
-## to do that, the functions should be able to
-## cache its inverse and create a special matrix object
-## to get the matrix, the function should be makeCacheMatrix
-## it is said that this function creates a
+## The first thing to do is to cache the inverse of a matrix
+## the functions should be able to cache its inverse and 
+## create a special matrix object.
+## The function should be makeCacheMatrix,
+## And to get the matrix
+## this function is said to create a
 ## special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()){
   inv <- NULL
   set <- function(y){
-        x <<- y
-        inv <<- NULL
+      x <<- y
+      inv <<- NULL
   }
   get <- function() {x}
   setInverse <- funtion(inverse) {inv <<- inverse}
@@ -17,19 +18,20 @@ makeCacheMatrix <- function(x = matrix()){
   list(set = set, get = get, setInverse = setInverse, getInverse =getInverse)
 }
 
-## the next function should be cacheSolve
-## this function calculates the inverse that is 
+## cacheSolve is the next function,
+## and it calculates the inverse that is 
 ## returned by makeCacheMatrix above.
-## to retrive the inverse of the cache, we need
-## to observe if the matrix has not been change after
+## We need to observe if the matrix
+## has not been change after
 ## the inverse of the special "matrix"
-## object has been calculated.
+## object has been calculated
+## to retrive the inverse of the cache, 
 
-cachesolve <- function(x, ...){
+cacheSolve <- function(x, ...){
     inv <- x$getInverse()
     if(!is.null(inv)){
-          message("getting cached data")
-          return(inv)
+        message("getting cached data")
+        return(inv)
     }
     mat <- x$get()
     inv <- solve(mat, ...)
